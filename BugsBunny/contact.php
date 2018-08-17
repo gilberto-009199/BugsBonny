a43c8978e7650293411187465ddea53fe8d8a29a
administrador <administrador@openserver-pc>
2018-08-17 16:33:53 -0300

    arrumando a pagina de Fale conosco front end para w3c

--- BugsBunny/contact.php
+++ BugsBunny/contact.php
@@ -2,7 +2,7 @@
 <html lang="pt-br" dir="ltr">
     <head>
         <title>Fale Conosco</title>
-<?php include_once './head.php';?>
+        <?php include_once './head.php'; ?>
     </head>
     <body>
         <header>
@@ -19,49 +19,49 @@
                 </nav>
             </div>
             <div class="ItemCaixaHeader">
-                    <div class="Logo">
-                        <img alt=" Logo do site" title="logo" height="32" width="32" src="img/logo2.png">
-                       <p>BugBunny</p>
-                    </div>
+                <div class="Logo">
+                    <img alt=" Logo do site" title="logo" height="32" width="32" src="img/logo2.png">
+                    <p>BugBunny</p>
                 </div>
+            </div>
         </header>
         <div role="banner">
             <script src="libs/Chart.js/Chart.js"></script>
             <canvas id="Grafico" height="300" width="1000">
-                               
+
             </canvas>
             <script>
-            var canvas = document.getElementById("Grafico");
-            var myChart = new Chart(canvas, {
-                type: 'line',
-                data: {
-                    labels:["janeiro","fevereiro","março","abril","maio","Junho","Julho","Agosto"],
-                    datasets:[{
-                            label:"Ultimos envios - 5 meses",
-                            data:[12,24,13,9,10,16,7,17],
-                            borderWidth:7,
-                            borderColor:"#454df0",
-                            backgroundColor:"transparent",
-                    },{
-                            label:"Atentimentos - 5 meses",
-                            data:[12,10,13,8,11,10,2,15],
-                            borderWidth:7,
-                            borderColor:"#006D5C",
-                            backgroundColor:"#00ffeb",
-                    }]
-                },
-                options: {
-                    title:{
-                        display:true,
-                        text:"Chamados"
+                var canvas = document.getElementById("Grafico");
+                var myChart = new Chart(canvas, {
+                    type: 'line',
+                    data: {
+                        labels: ["janeiro", "fevereiro", "março", "abril", "maio", "Junho", "Julho", "Agosto"],
+                        datasets: [{
+                                label: "Ultimos envios - 5 meses",
+                                data: [12, 24, 13, 9, 10, 16, 7, 17],
+                                borderWidth: 7,
+                                borderColor: "#454df0",
+                                backgroundColor: "transparent",
+                            }, {
+                                label: "Atentimentos - 5 meses",
+                                data: [12, 10, 13, 8, 11, 10, 2, 15],
+                                borderWidth: 7,
+                                borderColor: "#006D5C",
+                                backgroundColor: "#00ffeb",
+                            }]
+                    },
+                    options: {
+                        title: {
+                            display: true,
+                            text: "Chamados"
+                        }
                     }
-                }
-            });
+                });
             </script>
         </div>
         <div id="main" role="main">
             <div class="row">
-                <div class="cold6 BordaDireita" style="float: left;">
+                <div data-style="CaixaDireita" class="cold8 BordaDireita">
                     <form name="frmTiket" method="POST">
                         <table>
                             <tr>
@@ -78,7 +78,7 @@
                             </tr>
                             <tr>
                                 <td><label  class="Obrigatorio" for="txtEmail">E-mail:*</label></td>
-                                <td> <input id="txtHomePage" type="text" name="txtHomePage" value="" required/> </td>
+                                <td> <input id="txtEmail" type="text" name="txtHomePage" value="" required/> </td>
                             </tr>
                             <tr>
                                 <td><label for="txtHomePage">Home Page:</label></td>
@@ -96,7 +96,7 @@
                                         </tr>
                                         <tr>
                                             <td>
-                                                <textarea id="areaCritica" name="ariaCritica" rows="4" cols="20">
+                                                <textarea id="areaCritica" value="" name="ariaCritica" rows="4" cols="20">
                                                 </textarea>
                                             </td>
                                         </tr>
@@ -111,6 +111,7 @@
                                 <td><label class="Obrigatorio" for="slcSexo">Sexo:*</label></td>
                                 <td>
                                     <select id="slcSexo" name="slcSexo" required>
+                                        <option value="" selected>Sexo</option>
                                         <option value="M">M</option>
                                         <option value="F">F</option>
                                     </select>
@@ -120,6 +121,7 @@
                                 <td><label class="Obrigatorio" for="slcProfissao">Profissão:*</label></td>
                                 <td>
                                     <select id="slcProfissao" name="slcProfissao" required>
+                                        <option value="" selected>Profissão</option>
                                         <option value="cabeleireiro">Cabeleireiro</option>
                                         <option value="sapateiro">Sapateiro</option>
                                         <option value="vendedor">Vendedor</option>
@@ -135,13 +137,21 @@
                         </table>
                     </form>
                 </div>
-                <div class="cold3" style="float: left; padding-left: 20px;">
-                    Tipos de Ticket
+                <div data-style="CaixaEsquerda">
+                    Tipos de Tickets
+                    <span class="ItemTicket">Consulta</span>
+                    <span class="ItemTicket">Reportar Erro</span>
+                    <span class="ItemTicket">Pedido de Atualização</span>
+                    <span class="ItemTicket">Pedido de Alteração</span>
+                    <span class="ItemTicket">Contactar ADM</span>
+                    <span class="ItemTicket">Promoções</span>
+                    <span class="ItemTicket">Recuperar Boleto/2°via</span>
+                    <span class="ItemTicket">Reportar falha na Entrega</span>
                 </div>
             </div>            
         </div>
         <footer>
-            
+
         </footer>
     </body>
 </html>
