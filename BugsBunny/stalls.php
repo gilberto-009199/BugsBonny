796798023597ee90e7f4e9582753fb24489b54a2
administrador <administrador@openserver-pc>
2018-08-20 19:51:23 -0300

    verificando links para markets do mapa da pagina Nossas Bancas

--- BugsBunny/stalls.php
+++ BugsBunny/stalls.php
@@ -2,7 +2,7 @@
 <html lang="pt-br" dir="ltr">
     <head>
         <title>Nossas Bancas</title>
-        <?php include_once './head.php'; ?>
+<?php include_once './head.php'; ?>
     </head>
     <body>
         <header>
@@ -26,7 +26,7 @@
             </div>
         </header>
 
-        <div id="mapid" role="banner" style="margin-top: 80px; height: 260px; width: 100%; background-color: white;">
+        <div id="mapa" role="banner" style="margin-top: 80px; height: 260px; width: 100%; background-color: white;">
 
         </div>
         <link rel="stylesheet" href="./libs/leaflet/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="">
@@ -34,30 +34,30 @@
 
         <script>
             //setando o ponto de visualização de inicio do mapa para as cordenadas barueri
-            var mymap = L.map('mapid').setView([-23.512052, -46.881924], 13);
+            var map = L.map('mapa').setView([-23.512052, -46.881924], 13);
             //passando o parametro accesstoken com o token da minha conta para que o leaflet possa acessar o mapa do mapbox, alem de dar os direitos autoraris merecidos ao pessoal mantenedor do projeto!!
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
             {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a> ',
                 maxZoom: 18,/* mandando o zom padram como parametro*/
                 id: 'mapbox.streets-satellite',/* Passado como parametro ao mapbox.com  o tipo de mapa que nos queremos
                                         https://www.mapbox.com/api-documentation/#maps   */
-                accessToken: 'pk.eyJ1IjoiZ2lsYmVydG90ZWMiLCJhIjoiY2psMnF0eHNsMXRhODNrbDd5aGF3OXVwbiJ9.JUWwKVV_ZA-xNQFsIuvwQQ'}).addTo(mymap);
+                accessToken: 'pk.eyJ1IjoiZ2lsYmVydG90ZWMiLCJhIjoiY2psMnF0eHNsMXRhODNrbDd5aGF3OXVwbiJ9.JUWwKVV_ZA-xNQFsIuvwQQ'}).addTo(map);
             //L.marker([-23.512052, -46.881924]) adicionando um markert no mapa
-            var banca1 = L.marker([-23.512839, -46.874156]).addTo(mymap);
+            var banca1 = L.marker([-23.512839, -46.874156]).addTo(map);
             banca1.bindPopup("<h4 id='banca1'>Banca Santa Rica lotes:73/24/90</h4>\n\
                             <p>Estrada Municiapal n°123, centro, Barueri</p>");
-            var banca2 = L.marker([-23.502056, -46.86523]).addTo(mymap);
+            var banca2 = L.marker([-23.502056, -46.86523]).addTo(map);
             banca2.bindPopup("<h4 id='banca2'>Banca Monica lotes:70/10/32</h4>\n\
                             <p>Rua Topázio n°21, Aldeia, Barueri</p>");
-            var banca3 = L.marker([-23.512052, -46.881924]).addTo(mymap);
+            var banca3 = L.marker([-23.512052, -46.881924]).addTo(map);
             banca3.bindPopup("<h4 id='banca3'>Banca Maria José</h4>\n\
                             <p>Av. Carmine Gragnano n°457, Centro, Jandira</p>");
         </script>
         <div id="main" role="main">
             <!--    Enviando o visor do mapa para uma posição especifica com 14 de zoom    -->
-            <a href="#banca1" onclick="mymap.setView([-23.512839, -46.874156], 14);">banca 1</a>
-            <a href="#banca2" onclick="mymap.setView([-23.502056, -46.86523], 14);">banca 2</a>
-            <a href="#banca3" onclick="mymap.setView([-23.512052, -46.881924], 14);">banca 3</a>
+            <a href="#banca1" onclick="map.setView([-23.512839, -46.874156], 14);">banca 1</a>
+            <a href="#banca2" onclick="map.setView([-23.502056, -46.86523], 14);">banca 2</a>
+            <a href="#banca3" onclick="map.setView([-23.512052, -46.881924], 14);">banca 3</a>
         </div>
         <footer>
 
