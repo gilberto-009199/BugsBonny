9d94b89e28f1da117b23992c7c8fe68ce5fbba4b
administrador <administrador@openserver-pc>
2018-08-20 18:38:14 -0300

    criando a pagina nossas bancas e adicionando a biblioteca leaflet.js ao projeto

--- BugsBunny/stalls.php
+++ BugsBunny/stalls.php
@@ -2,7 +2,7 @@
 <html lang="pt-br" dir="ltr">
     <head>
         <title>Nossas Bancas</title>
-<?php include_once './head.php';?>
+        <?php include_once './head.php'; ?>
     </head>
     <body>
         <header>
@@ -19,21 +19,32 @@
                 </nav>
             </div>
             <div class="ItemCaixaHeader">
-                    <div class="Logo">
-                        <img alt=" Logo do site" title="logo" height="32" width="32" src="img/logo2.png">
-                       <p>BugBunny</p>
-                    </div>
+                <div class="Logo">
+                    <img alt=" Logo do site" title="logo" height="32" width="32" src="img/logo2.png">
+                    <p>BugBunny</p>
                 </div>
+            </div>
         </header>
-        <div role="banner">
-            
+        
+        <div id="mapid" role="banner" style="margin-top: 80px; height: 260px; width: 100%; background-color: white;">
+
         </div>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.3/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="">
+        <script src="./js/leaflet.js"></script>
+        
+         <script>
+           var mymap = L.map('mapid').setView([-23.512052, -46.881924], 13);
+           L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a> ', maxZoom: 18, id: 'mapbox.streets', accessToken: 'pk.eyJ1IjoiZ2lsYmVydG90ZWMiLCJhIjoiY2psMnF0eHNsMXRhODNrbDd5aGF3OXVwbiJ9.JUWwKVV_ZA-xNQFsIuvwQQ' }).addTo(mymap);
+           var banca1 = L.marker([-23.512052, -46.881924]).addTo(mymap);
+           banca1.bindPopup("<h4>Rua margarida n°446 , Jd. Belval, Barueri</h4>\n\
+                             <p>Banca Santa Rica lotes:73/24/90</p>");
+        </script>
         <div id="main" role="main">
-            
-            
+
+
         </div>
         <footer>
-            
+
         </footer>
     </body>
 </html>
