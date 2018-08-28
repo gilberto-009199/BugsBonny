5bd8534273d717c8d70be79d051c691bb1de8f4a
administrador <administrador@openserver-pc>
2018-08-27 21:30:47 -0300

    CRIANDO E DESENVOLVENDO A ESTRUTURA DA PAGINA Noticias(news.php)

--- BugsBunny/js/lib.js
+++ BugsBunny/js/lib.js
@@ -19,3 +19,27 @@ function ajax(){
     }
     return ObjetoAjax;
 }
+function sendAjax(parametros,url,type){
+    this.success;//função que ocorera se tudo correr bem
+    this.error;//função que ocorera quando tudo der errado!!kkk
+    this.url = url;
+    this.type= type; // get ou post
+    this.parametros = "{"+parametros+"}";
+    this.setSuccess= new function (metodo){
+        this.success = metodo;
+    };
+    this.setError= new function (metodo){
+        this.error = metodo;
+    };
+    this.enviar = new function(){
+        $.ajax({
+            url:this.url,
+            type:this.type,
+            dataType:'html',
+            data:this.parametros,
+            success:this.success,
+            error:this.error
+        });
+    };
+    
+}
--- BugsBunny/news.php
+++ BugsBunny/news.php
@@ -31,7 +31,7 @@
             </div>
         </header>
         <div role="banner">
-            <div class="row" style="height: 500px;">
+            <div class="row">
                 <style>
                     div[data-style="BarraPesquisa"]{
                         background: linear-gradient(265deg, rgba(255,255,255,1) 38%, rgba(231,227,227,1) 67%);
@@ -56,8 +56,9 @@
                         i[data-style="MenuPesquisa"]:hover>nav{
                             display:block;
                             position: absolute;
-                            left: 0;
-                            width: 100%;
+                            max-width: 100%;
+                            margin-left: -24px;
+                            width: 1200px;
                             z-index: 999;
                             border-top: solid 2px;
                             box-shadow: 2px 2px 2px black;
@@ -90,7 +91,7 @@
                                                 </div>
                                             </div>
                                             <div class="cold3">
-                                                <input  class="btn" type="submit">
+                                                <button class="btn" type="submit"> Pesquisar</button>
                                             </div>
                                         </form>
                                     </div>
@@ -100,21 +101,47 @@
                     </div>                    
                 </div>
                 <div class="row">
-                    <div class="cold5">
+                    <div class="cold6" style="float:left;">
                         <link rel="stylesheet" href="libs/Blink-Slider/blink.css">
                         <div id="CaixaSlider" style="margin-left: 21px;">
                             <section class="blink-slider">            
-                                <button id="prev" aria-label="voltar Imagem" style="position: absolute; z-index: 990; height: 91px; margin-top: 110px;">&lt;</button>
-                                <button id="next" aria-label="proxima Imagem"style="position: absolute; z-index: 990; height: 91px; margin-top: 110px; right: 0;">></button>
+                                <button id="prev" aria-label="voltar Imagem" style="    position: absolute;
+                                        z-index: 990;
+                                        height: 91px;
+                                        margin-top: 110px;
+                                        background: white;
+                                        color: black;
+                                        font-weight: bolder;
+                                        font-size: 22px;
+                                        border: none;
+                                        opacity: 0.2;
+                                        }">&lt;</button>
+                                <button id="next" aria-label="proxima Imagem" style=" position: absolute;
+                                        z-index: 990;
+                                        height: 91px;
+                                        margin-top: 110px;
+                                        right: 0;
+                                        background: white;
+                                        color: black;
+                                        font-weight: bolder;
+                                        font-size: 22px;
+                                        border: none;
+                                        opacity: 0.2;">></button>
                                 <div class="blink-view" id="blink">
                                     <div class="viewSlide">
-                                        <div class="ItemSlider" style="height: 300px; background-image: url(img/slide/banca.jpg);">
+                                        <div class="ItemSlider" style="background-repeat: no-repeat; background-size: cover; height: 300px; background-image: url(img/noticias/24rohingya-rakhine-1-threeByTwoSmallAt2X.jpg);">
 
                                         </div>
                                     </div>
 
                                     <div class="viewSlide">
-                                        <div class="ItemSlider" style="height: 300px;  background-image:url(img/slide/banca45.jpg);">
+                                        <div class="ItemSlider" style="background-repeat: no-repeat; background-size: cover;  height: 300px;  background-image:url(img/noticias/merlin_142457877_9ed7f2d3-7282-4538-8bdf-6aef22fec8b7-threeByTwoLargeAt2X.jpg);">
+
+                                        </div>
+                                    </div>
+
+                                    <div class="viewSlide">
+                                        <div class="ItemSlider" style="background-repeat: no-repeat; background-size: cover;  height: 300px;  background-image:url(img/noticias/xxbees-slide-KN6T-threeByTwoLargeAt2X.jpg);">
 
                                         </div>
                                     </div>
@@ -129,17 +156,16 @@
                                 </script>
                             </section>
                         </div>
-                        <p> Olá </p>
                     </div>
-                    <div class="cold5" style=" float: left; background-color:white; height: 330px;">
+                    <div class="cold4" style="padding-left: 10px; margin-left: 82px;  float:left; background-color:white; height: 330px;">
                         <section>
                             <h1> Ultimos topicos!</h1>
-                            <p>Lula Perde jugamento</p>
-                            <p>Bolsonaro perde para Ciro Gomes</p>
-                            <p>Banco Centralk declara falencia!!</p>
-                            <p>Numero de Desenpregados sobe para 19 milhões</p>
-                            <p>Donald Tramp é morto por milicia norte-coreana</p>
-                            <p>Ditador Chichaum e morto por ministro das armas</p>
+                            <p><i class="fas fa-balance-scale" style="margin-right: 4px;"></i>Lula perde jugamento</p>
+                            <p><i class="fas fa-balance-scale" style="margin-right: 4px;"></i>Bolsonaro perde para Ciro Gomes</p>
+                            <p><i class="far fa-money-bill-alt" style="margin-right: 4px;"></i>Banco Central declara falencia!!</p>
+                            <p><i class="far fa-money-bill-alt" style="margin-right: 4px;"></i>Numero de Desenpregados sobe para 19 milhões</p>
+                            <p><i class="fas fa-balance-scale" style="margin-right: 4px;"></i>Donald Tramp é morto por milicia norte-coreana</p>
+                            <p><i class="fas fa-balance-scale" style="margin-right: 4px;"></i>Ditador Chichaum e morto por ministro das armas</p>
                         </section>
 
                     </div>
@@ -147,8 +173,29 @@
             </div>
         </div>
         <div id="main" role="main">
-
-
+            <section>
+                <h1>Ultimas Notícias</h1>
+                <article>
+                    <h2>Asteroide maior do que Grande Pirâmide egípcia está se aproximando da Terra</h2>
+                    <div class="CaixaTexto"> 
+                        <p>Um enorme asteroide passará perto da Terra em 29 de agosto a uma velocidade de nove quilômetros por segundo, informou o Laboratório de Propulsão a Jato da NASA.</p>
+                        <p>O corpo celeste, denominado 2016 NF23, é considerado pela agência espacial como "potencialmente perigoso" devido a seu tamanho: seu diâmetro pode atingir entre 70 e 160 metros. O tamanho do asteroide pode ser comparado, dependendo de suas dimensões definitivas, com um avião Boeing 747 ou com a Grande Pirâmide de Giza (139 metros de altura).</p>
+                        <p>A agência estima que o asteroide passará a 4,8 milhões de quilômetros da Terra o que equivale a três vezes a distância entre a Terra e a Lua, segundo o Fox News.</p>
+                        <p>Segundo a classificação da NASA, qualquer corpo celeste que passe a uma distância menor que 7,5 milhões de quilômetros da Terra e tenha um diâmetro superior a 140 metros está na lista de corpos perigosos.</p>
+                        <p>O asteroide deverá passar pelo nosso planeta por volta da meia-noite, horário GMT, na próxima quarta-feira (29) (21h00, horário de Brasília).</p>
+                    </div>
+                </article>
+                <article>
+                    <h2>Motores da epidemia de obesidade no Brasil</h2>
+                    <div class="CaixaTexto"> 
+                        <p>Se o ritmo atual de crescimento da obesidade no Brasil for mantido, o país poderá apresentar em 2020 uma tendência de prevalência semelhante à dos Estados Unidos e do México, com excesso de peso em 35% da população.</p>
+                        <p>A avaliação foi feita por pesquisadores participantes do evento com o tema “Obesidade” no Ciclo de Palestras ILP-FAPESP, realizado no dia 20 de agosto na Assembleia Legislativa do Estado de São Paulo (Alesp).</p>
+                        <p>A prevalência de obesidade no Brasil se intensificou a partir dos anos 2000 e mudanças no padrão alimentar da população contribuem para a escalada do problema. Nas últimas décadas, o brasileiro passou a substituir alimentos tradicionais, como arroz, feijão e salada, por preparações ultraprocessadas.</p>
+                        <p>“Houve uma intensificação de um ambiente alimentar obesogênico [que causa obesidade] que influenciou o estilo de vida e contribuiu para o aumento do problema no país”, disse Patricia Constante Jaime, professora do Departamento de Nutrição da Faculdade de Saúde Pública da Universidade de São Paulo (FSP-USP).</p>
+                        <p>De acordo com a mais recente Pesquisa Nacional de Saúde publicada pelo Instituto Brasileiro de Geografia e Estatística (IBGE), 20,8% da população adulta brasileira – 26 milhões de pessoas – está obesa. A prevalência desse problema de saúde tem sido registrada em todas as faixas etárias e níveis de renda e em maior proporção em mulheres do que homens.</p>
+                    </div>
+                </article>
+            </section>  
         </div>
         <footer>
 
