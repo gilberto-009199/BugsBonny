75543e321b67c9109602ad77d484901b764bae2b
16254850 <16254850@DSN1271056558.cfp127aluno.local>
2018-09-10 11:17:07 -0300

    criando pagina ajuda(help.php)

---
+++ help.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome</title>
+    </head>
+    <body style="background: rgb(207,207,207);
+          background: linear-gradient(22deg, rgba(207,207,207,1) 45%, rgba(255,255,255,1) 100%);">
+        <div style="margin-left: auto; margin-right: auto; width: 800px; background-color: white;">
+            <h1>Por Favor! Avise o administrador pelo e-mail:</h1>
+            <p style="text-align: center;"><a href="mailto:gilberto.tec@vivaldi.net">gilberto.tec@vivaldi.net</a></p>
+            <h2>Tecnologias:</h2>
+            <table>
+                <tr>
+                    <td><img src="./img/logos/mariadb.png" height="200" alt="Mariadb"></td>
+                    <td><img src="./img/logos/html5.png" height="200"  alt="Html5"></td>
+                    <td><img src="./img/logos/css.png" height="200"  alt="css"></td>
+                    <td><img src="./img/logos/js.png" height="200"  alt="css"></td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+                        <img style="margin-left: auto; margin-right: auto; display: block;" src="./img/logos/php.png" height="200"  alt="php">
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
