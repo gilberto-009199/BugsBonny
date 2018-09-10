2340e807fefd1293b15c74bfc3e744ad8638f29b
administrador <administrador@openserver-pc>
2018-09-10 15:47:01 -0300

    retirando erros de escrita

--- celebrities.php
+++ celebrities.php
@@ -92,7 +92,7 @@
                 <article tabindex="0">
                     <h2> Entrevista com Carlos Alberto de Nóbrega antes do casamento.</h2>
                     <div class="CaixaTexto"> 
-                        <p>O humorista Carlos Alberto de Nóbrega está feliz da vida. Embreve se casará com a médica Renata Domingues no civil, o apresentador comemorará a união em uma cerimônia íntima no restaurante Trio Dezenove, localizado na Vila Olímpia.</p>
+                        <p>O humorista Carlos Alberto de Nóbrega está feliz da vida. Em breve se casará com a médica Renata Domingues no civil, o apresentador comemorará a união em uma cerimônia íntima no restaurante Trio Dezenove, localizado na Vila Olímpia.</p>
                         <button class="Direita">Ver Mais+</button>
                     </div>
                 </article>
@@ -106,7 +106,7 @@
                 <article tabindex="0">
                     <h2> Entrevista com Carlos Alberto de Nóbrega antes do casamento.</h2>
                     <div class="CaixaTexto"> 
-                        <p>O humorista Carlos Alberto de Nóbrega está feliz da vida. Embreve se casará com a médica Renata Domingues no civil, o apresentador comemorará a união em uma cerimônia íntima no restaurante Trio Dezenove, localizado na Vila Olímpia.</p>
+                        <p>O humorista Carlos Alberto de Nóbrega está feliz da vida. Em breve se casará com a médica Renata Domingues no civil, o apresentador comemorará a união em uma cerimônia íntima no restaurante Trio Dezenove, localizado na Vila Olímpia.</p>
                         <button class="Direita">Ver Mais+</button>
                     </div>
                 </article>
@@ -120,7 +120,7 @@
                 <article tabindex="0">
                     <h2> Entrevista com Carlos Alberto de Nóbrega antes do casamento.</h2>
                     <div class="CaixaTexto"> 
-                        <p>O humorista Carlos Alberto de Nóbrega está feliz da vida. Embreve se casará com a médica Renata Domingues no civil, o apresentador comemorará a união em uma cerimônia íntima no restaurante Trio Dezenove, localizado na Vila Olímpia.</p>
+                        <p>O humorista Carlos Alberto de Nóbrega está feliz da vida. Em breve se casará com a médica Renata Domingues no civil, o apresentador comemorará a união em uma cerimônia íntima no restaurante Trio Dezenove, localizado na Vila Olímpia.</p>
                         <button class="Direita">Ver Mais+</button>
                     </div>
                 </article>
@@ -134,7 +134,7 @@
                 <article tabindex="0">
                     <h2> Entrevista com Carlos Alberto de Nóbrega antes do casamento.</h2>
                     <div class="CaixaTexto"> 
-                        <p>O humorista Carlos Alberto de Nóbrega está feliz da vida. Embreve se casará com a médica Renata Domingues no civil, o apresentador comemorará a união em uma cerimônia íntima no restaurante Trio Dezenove, localizado na Vila Olímpia.</p>
+                        <p>O humorista Carlos Alberto de Nóbrega está feliz da vida. Em breve se casará com a médica Renata Domingues no civil, o apresentador comemorará a união em uma cerimônia íntima no restaurante Trio Dezenove, localizado na Vila Olímpia.</p>
                         <button class="Direita">Ver Mais+</button>
                     </div>
                 </article>
