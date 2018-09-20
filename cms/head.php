2aeb92bb9dbc7fff08fdee9ab21a965da60fc411
administrador <administrador@openserver-pc>
2018-09-20 18:38:58 -0300

    Criando a estrutura das paginas do cms

---
+++ cms/head.php
@@ -0,0 +1,12 @@
+<?php $arquivo=basename($_SERVER['PHP_SELF'],'.php');?>
+        <meta charset="utf-8">
+        <meta name="generator" content="netbeans">
+        <meta name="author" content="Gilberto Oliveira">
+        <meta name="copyright" content="Senai-SP">
+        <meta name="contactNetworkAddress" content="gilberto.tec@vivaldi.net">
+        <meta name="robots" content= "all"><!-- Definindo a aranha ou melhor motores de busca para indexarem todo o site e suas paginas-->
+        <script type="text/javascript" src="./libs/jquery/jquery-3.3.1.js"></script>
+        <script type="text/javascript" src="./libs/jqueryUI/jquery-ui.min.js"></script>
+        <link rel="icon" type="image/png" href="../img/logo2.png" />
+        <link rel="stylesheet" type="text/css" href="<?="./css/".$arquivo.".css"?>">
+    
---
+++ cms/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+    <head>
+        <?php require_once("./head.php") ?>
+        <title></title>
+    </head>
+    <body>
+        <div id="CaixaSite"><!-- Caixa que contem o conteudo do site -->
+            <header>
+                <div style="width:7%; display:inline-block;">
+                    <h1>CMS<h1>
+                </div>
+                <div class="cold4">
+                     <p style="font-size: 23px;">- Sistema de Gerenciamento do Site</p>
+                </div>
+                <div class="cold4 Direita" style="background: transparent; height: 77px; width: 231px; margin-top: 13px; margin-right: 78px;">
+                    <img alt="Logo" class="Direita" src="../img/logo2.png" width="64" height="64">
+                </div>
+            </header>
+            <div id="main" role="main">
+                <div class="row">
+                    <nav class="cold8 Esquerda" style="min-height: 85px; background: rgb(255,255,255); background: linear-gradient(265deg, rgba(255,255,255,1) 38%, rgba(231,227,227,1) 67%);">
+                        <div class="row">
+                            <div class="cold3 Esquerda" style="height: 85px; background: transparent; outline: #0078A8 solid 1px">
+                                <a href="" style="text-align: center; width: 100%; margin-left: auto; margin-right: auto; display: block;">
+                                    <img style="display: block; margin-left: auto; margin-right: auto;" src="img/content-icon.png" alt="Imagem Contêudo">
+                                    Adm. Conteúdo
+                                </a>
+                            </div>
+                            <div class="cold3 Esquerda" style="height: 85px; background: transparent; outline: #0078A8 solid 1px">
+                                <a href="" style="text-align: center; width: 100%; margin-left: auto; margin-right: auto; display: block;">
+                                    <img style="display: block; margin-left: auto; margin-right: auto;" src="img/User-Chat-icon.png" alt="Imagem Contêudo">
+                                    Adm. Fale Conosco
+                                </a>
+                            </div>
+                            <div class="cold3 Esquerda" style="height: 85px; background: transparent; outline: #0078A8 solid 1px">
+                                <a href="" style="text-align: center; width: 100%; margin-left: auto; margin-right: auto; display: block;">
+                                    <img style="display: block; margin-left: auto; margin-right: auto;" src="img/Emails-Folder-icon.png" alt="Imagem Contêudo">
+                                    Adm. Produtos
+                                </a>
+                            </div>
+                            <div class="cold3 Esquerda" style="height: 85px; background: transparent; outline: #0078A8 solid 1px">
+                                <a href="" style="text-align: center; width: 100%; margin-left: auto; margin-right: auto; display: block;">
+                                    <img style="display: block; margin-left: auto; margin-right: auto;" src="img/Windows-Messenger-icon.png" alt="Imagem Contêudo">
+                                    Adm. Usuarios
+                                </a>
+                            </div>
+                        </div>
+                     </nav>
+                    <div class="cold3 Esquerda" style="height: 85px; outline: #0078A8 solid 1px">
+                        <div class="row" style="padding-left: 27px; font-weight: bolder;">
+                            <p>Bem vindo, UserDefult.</p>
+                        </div>
+                        <div class="row">
+                            <div class="cold7" style="float: left; height: 31px;"></div>
+                            <div class="cold4" style="float: left;"><a style="font-size: 23px;"><i class="fas fa-sign-out-alt"></i>Logout</a></div>
+                        </div>
+                    </div>
+                </div>
+             </div>
+            <footer>
+
+
+            </footer>
+        </div>
+    </body>
+</html>
